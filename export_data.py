00f84df9927322a71344cde400433e07569ddb4c
Kevin Sallee <kevin.sallee@sano.co>
2019-11-05 20:13:59 -0600

    fixed bug

--- export_data.py
+++ export_data.py
@@ -13,13 +13,14 @@ def export_exif_data(folder_path, json_filepath):
     export_data = {}
     for f in os.listdir(folder_path):
         full_path = os.path.join(folder_path, f)
+        image_name = os.path.splitext(f)[0]
         pil_image = Image.open(full_path)
         width, height = pil_image.size
-        export_data[full_path] = {"width": width, "height": height}
+        export_data[image_name] = {"width": width, "height": height}
         exif_data = pil_image._getexif()
         if exif_data:
             for (k, v) in exif_data.items():
-                export_data[full_path][f"{TAGS.get(k)}"] = f"{v}"
+                export_data[image_name][f"{TAGS.get(k)}"] = f"{v}"
     with open(json_filepath, "w") as w_handle:
         json.dump(export_data, w_handle, indent=4, sort_keys=True)
     print(f"Exported data to {json_filepath}. Have an awesome day!")
