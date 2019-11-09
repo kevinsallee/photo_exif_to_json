75dd07e899e2d530aa2866f1dfbabd52d334906a
Kevin Sallee <kevin.sallee@gpltech.com>
2019-11-09 10:51:06 -0600

    added try except

--- export_data.py
+++ export_data.py
@@ -5,6 +5,9 @@ import click
 from PIL import Image
 from PIL.ExifTags import TAGS
 
+import logging
+logging.basicConfig()
+logger = logging.getLogger(__name__)
 
 @click.command()
 @click.argument("folder_path")
@@ -13,15 +16,19 @@ from PIL.ExifTags import TAGS
 def export_exif_data(folder_path, json_filepath, wh):
     export_data = {}
     for f in os.listdir(folder_path):
-        full_path = os.path.join(folder_path, f)
-        image_name = os.path.splitext(f)[0]
-        pil_image = Image.open(full_path)
-        width, height = pil_image.size
-        export_data[image_name] = {"width": width, "height": height}
-        exif_data = pil_image._getexif()
-        if exif_data and not wh:
-            for (k, v) in exif_data.items():
-                export_data[image_name][f"{TAGS.get(k)}"] = f"{v}"
+        try:
+            full_path = os.path.join(folder_path, f)
+            image_name = os.path.splitext(f)[0]
+            pil_image = Image.open(full_path)
+            width, height = pil_image.size
+            export_data[image_name] = {"width": width, "height": height}
+            exif_data = pil_image._getexif()
+            if exif_data and not wh:
+                for (k, v) in exif_data.items():
+                    export_data[image_name][f"{TAGS.get(k)}"] = f"{v}"
+        except Exception as e:
+            logger.warning(f"Problem with {f}: {e}")
+
     with open(json_filepath, "w") as w_handle:
         json.dump(export_data, w_handle, indent=4, sort_keys=True)
     print(f"Exported data to {json_filepath}. Have an awesome day!")
